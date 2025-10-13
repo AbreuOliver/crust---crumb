ba24f32b502249a0e6cf8e6e405778cf272c40bf
Oliver Abreu <oliverabreu@icloud.com>
2025-10-13 16:57:23 -0400

    trying to prevent overscroll

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,15 @@
 </script>
 
 
-<section class="h-dvh overflow-y-auto overscroll-x-none overflow-x-hidden" style="-webkit-overflow-scrolling: touch; smooth-scrolling">
+<!-- <section class="app-scroll h-dvh overflow-y-auto overscroll-x-none overflow-x-hidden" style="-webkit-overflow-scrolling: touch; smooth-scrolling">
 	<Header />
 	{@render children()}
+</section> -->
+
+<section
+  class="app-scroll overflow-y-auto overflow-x-hidden overscroll-contain"
+  style="-webkit-overflow-scrolling: touch; scroll-behavior: smooth"
+>
+  <Header />
+  {@render children()}
 </section>
