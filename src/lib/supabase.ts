5791a1ff7fe1c506bd437c89c53126c24ed533ad
Oliver Abreu <oliverabreu@icloud.com>
2025-10-12 22:11:20 -0400

    fixes auth error, hopefully

--- src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr';
-
-export const supabaseBrowser = createBrowserClient(
-	import.meta.env.PUBLIC_SUPABASE_URL!,
-	import.meta.env.PUBLIC_SUPABASE_ANON_KEY!
-);
